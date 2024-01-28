208f7da83c1ffc9a222df3ef37e786fa812ec4d8
inkay_liu <110617789+sebandx@users.noreply.github.com>
2024-01-28 17:03:08 -0500

    I messed smth up while trying to clean up

--- Assets/Distant Lands/Thalassophobia - Low Poly Oceans/Contents/Scripts/CoralDescription.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class CoralDescription : MonoBehaviour {
-    public string description;
-
-}
-
--- Assets/Distant Lands/Thalassophobia - Low Poly Oceans/Contents/Scripts/ObjectInfo.cs
+++
@@ -1,7 +0,0 @@
-// ObjectInfo.cs
-using UnityEngine;
-
-public class ObjectInfo : MonoBehaviour
-{
-    public string info = "Default Info"; // You can set this in the Inspector for each object
-}
